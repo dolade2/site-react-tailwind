7c03fda90119b26ecd488d6ce4813941c6d333f1
dolade2 <105802029+dolade2@users.noreply.github.com>
2023-09-14 16:11:33 -0300

    atualização

---
+++ src/Main/js/EventoCards.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function EventoButtonCard({setShowCards, showCards}){
+     
+    return(
+        <div onClick={(e)=>setShowCards(!showCards)}>
+         
+        </div>
+    )        
+
+} export default EventoButtonCard
